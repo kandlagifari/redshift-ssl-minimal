6ba645a3110732c71f21ef3eb8abfd5313cef4c2
Kandla Gifari Akbar <kandlagifari@gmail.com>
2024-02-15 15:09:45 +0700

    change record type

--- terraform/08_private-hosted-zone.tf
+++ terraform/08_private-hosted-zone.tf
@@ -9,7 +9,7 @@ resource "aws_route53_zone" "redshift-r53" {
 resource "aws_route53_record" "redshift-ns" {
   zone_id = aws_route53_zone.redshift-r53.zone_id
   name    = "redshift-ssl.kafuuchino.com"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 300
   records = [module.redshift-ssl-cluster.cluster_dns_name]
 }
